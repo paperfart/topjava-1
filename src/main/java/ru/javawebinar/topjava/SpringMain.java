fa59fbf4174f402786e81739f480f535d16107c4
igor <head4work@gmail.com>
2021-10-22 12:03:25 -0700

    MealTo methods added to MealRestController

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,9 +2,12 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Arrays;
 
 public class SpringMain {
@@ -16,6 +19,10 @@ public class SpringMain {
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
             mealRestController.getAll().forEach(System.out::println);
             // adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+            mealRestController.create(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0),
+                    "Завтрак", 500));
+            mealRestController.get(8).setUserId(2);
+            mealRestController.get(8);
         }
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalTime;
 import java.util.Collection;
+import java.util.List;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 
@@ -43,5 +47,16 @@ public class MealRestController {
     public Collection<Meal> getAll() {
         log.info("getAll");
         return service.getAll(SecurityUtil.authUserId());
+    }
+
+    public List<MealTo> getAllTos() {
+        log.info("getAll with exceeded");
+        return MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public List<MealTo> getAllTosFilteredByTime(LocalTime startTime, LocalTime endTime) {
+        log.info("getAll with exceeded filtered by time");
+        return MealsUtil.getFilteredTos(service.getAll(SecurityUtil.authUserId()),
+                MealsUtil.DEFAULT_CALORIES_PER_DAY, startTime, endTime);
     }
 }
