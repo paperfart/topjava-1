74e9b758fe388353a1593ab17539b99b08a2bc3b
igor <head4work@gmail.com>
2022-06-26 11:54:21 -0700

    RootControllerTest getMeals done

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,13 +2,18 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -25,6 +30,25 @@ class RootControllerTest extends AbstractControllerTest {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 USER_MATCHER.assertMatch(actual, admin, user);
+                            }
+                        }
+                ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                List<Meal> meals = List.of(MealTestData.meal7, meal6, meal5, meal4, meal3, meal2, meal1);
+                                List<MealTo> mealTos = MealsUtil.filterByPredicate(meals, MealsUtil.DEFAULT_CALORIES_PER_DAY, i -> true);
+                                MEALTO_MATCHER.assertMatch(actual, mealTos);
                             }
                         }
                 ));
